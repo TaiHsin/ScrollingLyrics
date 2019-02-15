f57f415ab60d807cf2c4cf1e8064a4a3a61eaa22
taihsinlee <taihsinlee@kkbox.com>
2019-02-15 16:08:04 +0800

    Created xcode project

---
+++ ScrollingLyrics/ScrollingLyrics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollingLyrics
+//
+//  Created by Tsihsin Lee on 2019/2/15.
+//  Copyright © 2019 Tsihsin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScrollingLyrics/ScrollingLyrics/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ScrollingLyrics
+//
+//  Created by Tsihsin Lee on 2019/2/15.
+//  Copyright © 2019 Tsihsin Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
